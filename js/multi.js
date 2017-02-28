1b5815a8155414a589ec3d1c22fe3f1c2bc57ab5
yugang <anhao@192.168.1.103>
2017-02-28 11:42:44 +0800

    Site updated: 2017-02-28 11:42:44

--- js/multi.js
+++ js/multi.js
@@ -12,37 +12,9 @@ function IsPC(){
 function imagePreview(input){
 	var files = input.files;
 	var preview =document.getElementById("yg_multi");
-	//判断已经存在的图片个数
 	var l=$(".choosedImg div").length;
 	if(files.length+l>9){
 		$("textarea").val("您选的图片大于9张，请重新选择");
 		return false;
 	}
-	for (var i = 0; i < files.length; i++) {
-		//预览新添加的图片
-	    var file = files[i];
-		var div=document.createElement("div");
-		div.className="choosedImg";
-		var div2=document.createElement("div");
-		var text=document.createTextNode("X");
-	    var img = document.createElement("img");
-        img.file = file;
-		div.appendChild(img);
-		div2.appendChild(text);
-		div.appendChild(div2);
-        preview.insertBefore(div,preview.childNodes[0]);
-        var reader = new FileReader();
-        reader.onload = (function(aImg) {
-          return function(e) {
-		  	dele(e.target.result);
-            aImg.src = e.target.result;
-          };
-        })(img);
-        reader.readAsDataURL(file);
-    }
-}
-//图片去重
-function dele(ob){
-	var obj=$(".choosedImg img");
-	var yl=obj.length;
-	for(var i=0;i
+	for (var i = 0;i
